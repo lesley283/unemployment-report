8815ec768fa11d1ce7632f3e83bbf86ad6b98cc3
Lesley Lam <lyl13@georgetown.edu>
2022-11-07 17:39:13 -0500

    Test USD formatting logic

--- app/stocks.py
+++ app/stocks.py
@@ -1,36 +1,39 @@
 # this is the "app/stocks.py" file...
 
-print("STOCKS REPORT...")
-
 from pandas import read_csv
 from app.alpha import API_KEY
 
 def format_usd(my_price):
     return f"${my_price:,.2f}"
 
-symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
-print("SYMBOL:", symbol)
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+if __name__ == "__main__":
 
-df = read_csv(request_url)
-print(df.columns)
-print(df.head())
-#breakpoint()
+    print("STOCKS REPORT...")
 
-# CHALLENGE A:
-# print the latest closing date and price
+    symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
+    print("SYMBOL:", symbol)
 
-latest = df.iloc[0]
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
 
-#print(latest["timestamp"])
-#print(latest["close"])
-print("LATEST:", format_usd(latest["adjusted_close"]), "as of", latest["timestamp"])
+    df = read_csv(request_url)
+    print(df.columns)
+    print(df.head())
+    #breakpoint()
 
-# Challenge B
-#
-# What is the highest high price (formatted as USD)?
-# What is the lowest low price (formatted as USD)?
+    # CHALLENGE A:
+    # print the latest closing date and price
 
-print("HIGH:", format_usd(df["high"].max()))
-print("LOW:", format_usd(df["low"].min()))
+    latest = df.iloc[0]
+
+    #print(latest["timestamp"])
+    #print(latest["close"])
+    print("LATEST:", format_usd(latest["adjusted_close"]), "as of", latest["timestamp"])
+
+    # Challenge B
+    #
+    # What is the highest high price (formatted as USD)?
+    # What is the lowest low price (formatted as USD)?
+
+    print("HIGH:", format_usd(df["high"].max()))
+    print("LOW:", format_usd(df["low"].min()))
---
+++ test/stocks_test.py
@@ -0,0 +1,15 @@
+
+# this is the "test/stocks_test.py" file...
+
+from app.stocks import format_usd
+
+def test_usd_formatting():
+
+    assert format_usd(4.5) == "$4.50"
+
+    assert format_usd(4.55555555) == "$4.56"
+
+    assert format_usd(1234567890) == "$1,234,567,890.00"
+
+    # testing invalid inputs
+    # assert format_usd("OOPS") == "________"
