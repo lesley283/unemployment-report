e3d4b20d58a487626824072f24d649e800d320df
Lesley Lam <lyl13@georgetown.edu>
2022-11-07 17:55:10 -0500

    Refactor and tested stocks data fetching logic

--- app/stocks.py
+++ app/stocks.py
@@ -7,6 +7,14 @@ def format_usd(my_price):
     return f"${my_price:,.2f}"
 
 
+def fetch_stocks_data(symbol):
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+
+    df = read_csv(request_url)
+
+    return df
+
+
 if __name__ == "__main__":
 
     print("STOCKS REPORT...")
@@ -14,9 +22,8 @@ if __name__ == "__main__":
     symbol = input("Please input a crypto symbol (default: 'NFLX'): ") or "NFLX"
     print("SYMBOL:", symbol)
 
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={API_KEY}&datatype=csv"
+    df = fetch_stocks_data(symbol)
 
-    df = read_csv(request_url)
     print(df.columns)
     print(df.head())
     #breakpoint()
--- test/stocks_test.py
+++ test/stocks_test.py
@@ -1,7 +1,8 @@
 
 # this is the "test/stocks_test.py" file...
 
-from app.stocks import format_usd
+from app.stocks import format_usd, fetch_stocks_data
+from pandas import DataFrame
 
 def test_usd_formatting():
 
@@ -13,3 +14,15 @@ def test_usd_formatting():
 
     # testing invalid inputs
     # assert format_usd("OOPS") == "________"
+
+def test_data_fetching():
+
+    result = fetch_stocks_data("NFLX")
+    assert isinstance(result, DataFrame)
+
+    assert "timestamp" in result.columns
+    assert "adjusted_close" in result.columns
+    assert "high" in result.columns
+    assert "low" in result.columns
+
+    assert len(result) >= 100
